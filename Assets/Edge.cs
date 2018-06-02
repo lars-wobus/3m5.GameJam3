2d52726db292f1d3e69ded8ca2f4d4c25401ded1
ChristianK-TUD <ck-mail@gmx-topmail.de>
2018-06-02 15:43:57 +0200

    Graph generation

--- Assets/Edge.cs
+++ Assets/Edge.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Edge : MonoBehaviour {
+public class Edge : MonoBehaviour  {
 
     public Node start;
     public Node end;
@@ -14,13 +15,19 @@ public class Edge : MonoBehaviour {
         linedrawer.positionCount = 2;
         linedrawer.SetPosition(0, start.transform.position);
         linedrawer.SetPosition(1, end.transform.position);
-        Debug.Log(start.transform.position);
     }
 	
+    public void SetNodes(Node start, Node end)
+    {
+        this.start = start;
+        this.end = end;
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        linedrawer.SetPosition(0, start.transform.position);
+        linedrawer.SetPosition(1, end.transform.position);
+    }
 
     public void OnDrawGizmos()
     {
@@ -29,5 +36,10 @@ public class Edge : MonoBehaviour {
         Vector3 pos = start.transform.position;
         Vector3 pos2 = end.transform.position;
         Gizmos.DrawLine(pos, pos2);
+    }
+
+    public bool connectsTo(Node target)
+    {
+        return start == target || end == target;
     }
 }
---
+++ Assets/Graph.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+    public Transform[] prefabs;
+    public List<Transform> nodes;
+    public List<Edge> edges;
+    public Transform prefab_edge;
+
+    int num_edges = 7;
+
+
+    // Use this for initialization
+    void Start () {
+        nodes.Add(Instantiate(prefabs[0], new Vector3(0, 1, 0), Quaternion.identity));
+        nodes.Add(Instantiate(prefabs[0], new Vector3(2, 3, 0), Quaternion.identity));
+        nodes.Add(Instantiate(prefabs[0], new Vector3(2, 2, 0), Quaternion.identity));
+        nodes.Add(Instantiate(prefabs[0], new Vector3(4, 1, 0), Quaternion.identity));
+        nodes.Add(Instantiate(prefabs[0], new Vector3(4, 3, 0), Quaternion.identity));
+        nodes.Add(Instantiate(prefabs[0], new Vector3(6, 2, 0), Quaternion.identity));
+
+        for(int i = 0; i < num_edges; i++)
+            edges.Add(Instantiate(prefabs[1]).GetComponent<Edge>());
+        edges[0].SetNodes(nodes[0].GetComponent<Node>(), nodes[1].GetComponent<Node>());
+        edges[1].SetNodes(nodes[0].GetComponent<Node>(), nodes[2].GetComponent<Node>());
+        edges[2].SetNodes(nodes[1].GetComponent<Node>(), nodes[2].GetComponent<Node>());
+        edges[3].SetNodes(nodes[2].GetComponent<Node>(), nodes[3].GetComponent<Node>());
+        edges[4].SetNodes(nodes[2].GetComponent<Node>(), nodes[4].GetComponent<Node>());
+        edges[5].SetNodes(nodes[3].GetComponent<Node>(), nodes[5].GetComponent<Node>());
+        edges[6].SetNodes(nodes[4].GetComponent<Node>(), nodes[5].GetComponent<Node>());
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
