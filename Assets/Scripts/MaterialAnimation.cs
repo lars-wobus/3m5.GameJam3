72073711a07f3f1baf30bb1ffcceaab587e1666a
Lars Wobus <lars.wobus@web.de>
2018-06-03 09:33:34 +0200

    ADD simple texture animation for the blood stream

---
+++ Assets/Scripts/MaterialAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialAnimation : MonoBehaviour {
+
+    [SerializeField] private Material material;
+    [SerializeField] private Vector2 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        material.SetTextureOffset("_MainTex", offset * Time.time);
+    }
+}
