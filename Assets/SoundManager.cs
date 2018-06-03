50fec1bd232995619aad6f310469f435266374fe
ChristianK-TUD <ck-mail@gmx-topmail.de>
2018-06-03 17:22:09 +0200

    Blop sounds

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlopSoundManager : MonoBehaviour {
+
+    AudioSource blopsound;
+
+	// Use this for initialization
+	void Start () {
+        blopsound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void playBlobSound()
+    {
+        blopsound.Play();
+    }
+}
