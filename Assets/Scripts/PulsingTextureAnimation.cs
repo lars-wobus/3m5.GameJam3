d28ab5097e180e52fbbfee144c12269a112dfbe9
Lars Wobus <lars.wobus@web.de>
2018-06-03 18:21:36 +0200

    ADD pulsing material

---
+++ Assets/Scripts/PulsingTextureAnimation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PulsingTextureAnimation : MonoBehaviour {
+
+    [SerializeField] private Material material;
+    [SerializeField] private Material startMaterial;
+    [SerializeField] private Material stopMaterial;
+    [SerializeField] private float length = 3;
+
+    private Material Material
+    {
+        get
+        {
+            return material;
+        }
+    }
+
+    private Material StartMaterial
+    {
+        get
+        {
+            return startMaterial;
+        }
+    }
+
+    private Material StopMaterial
+    {
+        get
+        {
+            return stopMaterial;
+        }
+    }
+
+    private float Length
+    {
+        get
+        {
+            return length;
+        }
+    }
+
+    void Update () {
+        var value = Mathf.PingPong(Time.time, Length);
+        Debug.Log(value);
+        Material.Lerp(StartMaterial, StopMaterial, value / Length);
+    }
+}
