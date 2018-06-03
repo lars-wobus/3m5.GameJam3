f32a38f2480803981594bc6dfb575c909a7f54ea
Lars Wobus <lars.wobus@web.de>
2018-06-03 10:41:23 +0200

    UPDATE script resets offset on application quit

--- Assets/Scripts/MaterialAnimation.cs
+++ Assets/Scripts/MaterialAnimation.cs
@@ -1,19 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MaterialAnimation : MonoBehaviour {
 
     [SerializeField] private Material material;
     [SerializeField] private Vector2 offset;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
 	void Update () {
         material.SetTextureOffset("_MainTex", offset * Time.time);
+    }
+
+    private void OnApplicationQuit()
+    {
+        material.SetTextureOffset("_MainTex", Vector2.zero);
     }
 }
